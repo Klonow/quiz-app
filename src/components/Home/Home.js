c0993dfd306f7adf2698916837f7793aeca5b1eb
klon <sindikat633@gmail.com>
2020-12-04 13:54:39 +0300

    Added Home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import classes from './Home.module.css'
+
+export const Home = () => {
+    return (
+        <div className={classes.Home}>
+            <h1>Quizzes - Friday July 3, 2020</h1>
+            <p>Savich Egor, sindikat633@gmail.com </p>
+        </div>
+    )
+}
